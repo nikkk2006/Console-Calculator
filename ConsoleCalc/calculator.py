fa1597deb26da501a6e3e2d9d4e4609bd65dc1c4
Nikita Hlinski <hlinski220706@gmail.com>
2022-12-27 20:11:09 +0300

    fourth commit

--- ConsoleCalc/calculator.py
+++ ConsoleCalc/calculator.py
@@ -14,8 +14,21 @@ def int_div(numb_a: int, numb_b: int) -> float:
     return numb_a // numb_b
 
 def main():
+    OPERATIONS = ('+', '-', '*', '/', '//')
+
+    operation = input(f'Choose operation: {OPERATIONS}\n')
     number_a, number_b = [int(input(f"Enter number {number}:\n")) for number in range(1, 3)]
-    print(int_div(number_a, number_b))
+
+    if operation == '+':
+        print(add(number_a, number_b))
+    elif operation == '-':
+        print(sub(number_a, number_b))
+    elif operation == '*':
+        print(mult(number_a, number_b))
+    elif operation == '/':
+        print(div(number_a, number_b))
+    elif operation == '//':
+        print(int_div(number_a, number_b))
 
 
 if __name__ == '__main__':
