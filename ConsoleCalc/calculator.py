54b0ff45d6ad32738379eb1b7c5aab60a63383fd
Nikita Hlinski <hlinski220706@gmail.com>
2022-12-27 19:05:51 +0300

    second commit

--- ConsoleCalc/calculator.py
+++ ConsoleCalc/calculator.py
@@ -2,9 +2,11 @@
 def add(numb_a: int, numb_b: int) -> int:
     return numb_a + numb_b
 
+def sub(numb_a: int, numb_b: int) -> int:
+    return numb_a - numb_b
+
 def main():
-    number_a = int(input('Enter number a:\n'))
-    number_b = int(input('Enter number b:\n'))
+    number_a, number_b = [int(input(f"Enter number {number}:\n")) for number in range(1, 3)]
     print(add(number_a, number_b))
 
 
