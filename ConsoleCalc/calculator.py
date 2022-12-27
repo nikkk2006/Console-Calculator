de52d11124edb0d3af8f24286272253793c40c24
Nikita Hlinski <hlinski220706@gmail.com>
2022-12-27 19:56:04 +0300

    third commit

--- ConsoleCalc/calculator.py
+++ ConsoleCalc/calculator.py
@@ -1,13 +1,21 @@
-
 def add(numb_a: int, numb_b: int) -> int:
     return numb_a + numb_b
 
 def sub(numb_a: int, numb_b: int) -> int:
     return numb_a - numb_b
 
+def mult(numb_a: int, numb_b: int) -> int:
+    return numb_a * numb_b
+
+def div(numb_a: int, numb_b: int) -> float:
+    return numb_a / numb_b
+
+def int_div(numb_a: int, numb_b: int) -> float:
+    return numb_a // numb_b
+
 def main():
     number_a, number_b = [int(input(f"Enter number {number}:\n")) for number in range(1, 3)]
-    print(add(number_a, number_b))
+    print(int_div(number_a, number_b))
 
 
 if __name__ == '__main__':
